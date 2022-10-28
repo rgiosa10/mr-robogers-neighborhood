15b5e62b009c512ed5f8d2b4d965f2c2f021b00d
Ruben Giosa <rubenalexgiosa@gmail.com>
2022-10-28 11:43:41 -0700

    Add UI logic that shows results and reset the page, with some bootstrap and unique styling

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,7 @@ function modifyArray(numberArray) {
         if (element.charAt(j) === "3") {
           numberArray[i] = "Won't you be my neighbor?";
         } else if (element.charAt(j) === "2") {
-          numberArray[i] = "Beep!";
+          numberArray[i] = "Boop!";
         } else if (element.charAt(j) === "1") {
           numberArray[i] = "Beep!";
         }
@@ -38,22 +38,55 @@ function modifyArray(numberArray) {
     }
   }
   console.log(numberArray)
+  return numberArray;
 }
 
 // UI Logic
 
+function reset() {
+  // Unhide form submit button
+  document.getElementById("form-submit-button").setAttribute("class", "btn btn-primary btn-lg");
 
+  // Hide the results section and reset button
+  document.getElementById("result").setAttribute("class", "hidden");
+  document.getElementById("reset-btn").setAttribute("class", "hidden");
+
+  // Reset form inputs to default values
+  document.getElementById("text-input1").value = null;
+}
 
 window.addEventListener("load", function() {
   let form = document.querySelector("form");
+  let results = document.getElementById("resultsText");
+  let resetBtn = document.getElementById("reset-btn")
 
   form.addEventListener("submit", function(event) {
     event.preventDefault();
 
     const text = document.getElementById("text-input1").value;
+    let p = document.getElementById("resultText")
+    let numberArray = convertToArray(text);
+    let resultsFinal = modifyArray(numberArray)
+    
+    // For loop in order to show each result on the webpage
+    for (let i = 0; i < resultsFinal.length; i += 1) {
+      let ul = document.createElement("ul");
+      let li = document.createElement("li");
+      p.after(ul);
+      ul.append(li);
+      li.append("Number " + i + ": " + resultsFinal[i]);
+    }
 
-    numberArray = convertToArray(text);
-    modifyArray(numberArray)
+    // Unhide results
+    document.getElementById("result").setAttribute("class", "bottom");
+
+    // Hide the form submit button
+    document.getElementById("form-submit-button").setAttribute("class", "hidden");
+
+    // Unhide the reset button
+    document.getElementById("reset-btn").setAttribute("class","btn btn-outline-warning");
+    // Event Listener for clicking the reset button
+    resetBtn.addEventListener("click", reset);
 
   });
 });
