4e7f902cf375b95501a72d90225d66be0a3ed157
Ruben Giosa <rubenalexgiosa@gmail.com>
2022-10-28 08:43:49 -0700

    Add for loop to pass test 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 // Business Logic
 
 function convertToArray(text) {
-  textNumber = parseInt(text);
-  console.log(textNumber)
+  let textNumber = parseInt(text);
+  let numberArray = [0];
+  let counter = 0;
+  
+  for (let i = 1; i < 6; i += 1) {
+    counter += 1;
+    numberArray[i] = counter;
+  }
+  console.log(numberArray);
 }
 
 convertToArray("5");
